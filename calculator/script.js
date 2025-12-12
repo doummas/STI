c5b46fefacafa515b8ae89272483007ba84a92af
yaya <sammoud666@gmail.com>
2025-12-12 13:38:53 -0500

    all in one repo

--- calculator/script.js
+++ calculator/script.js
@@ -3,12 +3,16 @@ const display = document.getElementById("natija")
 const ce = document.getElementById("ce")
 
 function appendtd(input){
+    
     display.innerHTML += input
+    
 }
 
-function calculate(){
-    display.innerHTML = eval(display.innerHTML)
+function calculate(input){
+    console.log()
 }
 ce.onclick = function(){
     display.innerHTML =""
 }
+
+calculate()
---
+++ rev_js/js.js
@@ -0,0 +1,40 @@
+/*function saisir(){
+    var ch = prompt("Donner chaine de forme a+b")
+    return ch
+}
+function calculer(){
+    var a = Number(ch.substring(0,ch.indexOf("+")))
+    var b = Number(ch.substring(ch.indexOf("+") +1 ,ch.length))
+    ch1 = a+b
+    alert(ch1)
+}
+ch = saisir()
+calculer()*/
+
+
+
+
+function saisir(){
+    let str = prompt("Donner une ")
+    
+    return str
+}
+
+function  type(str){
+    if (isNaN(str) == false){
+        alert("number")
+    } 
+    else if (isNaN(str) == false && str. ){
+
+    }
+    else{
+        alert("symbole")
+    }
+
+
+}
+
+str= saisir()
+type(str)
+
+
