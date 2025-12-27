0703dccc5acc9f2adf4efb2268772a3168b1d2a5
doummas <sammoud666@gmail.com>
2025-12-27 16:46:41 +0100

    simple update

--- guessing_game/script.js
+++ guessing_game/script.js
@@ -1,24 +1,40 @@
 const ro_number = Math.round( 1 + Math.random() * 9 )
 
 console.log(ro_number)
+running = true
+function choose(){
 do{
-    var user_num = prompt("Choose a number")
+    window.user_num = prompt("Choose a number")
 }while (user_num > 10 || user_num < 1 )
+}
+function ela (u,r){
+    return u-r
+}
 
-var cond = user_num - ro_number
+choose()
 
-switch (true){
-    case ( cond < 3  && cond > 0):
+
+while (running){
+    switch (true){
+    case ( ela(user_num,ro_number) < 3  && ela(user_num,ro_number) > 0):
         alert("Number choosed is high")
+        choose()
         break
-    case ( cond > 3 && cond > 0 ):
+    case ( ela(user_num,ro_number) > 3 && ela(user_num,ro_number) > 0 ):
         alert("Number choosed is too high")
+        choose()
         break
-    case ( cond > -3 && cond < 0 ):
+    case ( ela(user_num,ro_number) > -3 && ela(user_num,ro_number) < 0 ):
         alert("Number choosed is low")
+        choose()
         break
-    case ( cond < -3 && cond < 0):
+    case ( ela(user_num,ro_number) < -3 && ela(user_num,ro_number) < 0):
         alert("Number choosed is too low")
+        choose()
+        break
     default:
         alert("The two numbers are equal")
+        running = false
+        break
+}
 }
