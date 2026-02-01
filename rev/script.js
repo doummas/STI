390705f059d1fa15ab673ccd187d1712e8dedec8
doummas <sammoud666@gmail.com>
2026-02-01 13:57:42 +0100

    oui

--- rev/script.js
+++ rev/script.js
@@ -1,6 +1,30 @@
-var code = document.getElementById("code")
-var rep = document.getElementById("rep")
+
 function generate(){
-    var code = document.getElementById("code")
+    let code = document.getElementById("code")
+    for (let i =0 ;i<10;i++){
+        code.value += String.fromCharCode(Math.round(97 + Math.random() *25 - (32 *Math.round(Math.random()))))
+    }
+}
+
+function valider(){
+    let code = document.getElementById("code")
+    let rep = document.getElementById("rep")
+    let maj = 0
+    for (let i = 0;i<code.value.length;i++){
+        if (code.value[i]<"Z" && code.value[i]> "A"){
+            maj+=1
+        }
+    
+
+    }
+    console.log(maj)
+    if (rep.value == maj){
+        alert("You're not a rorbot")
+    }
+    else{
+        alert("You're a robot")
+    }
+
+
 
 }
